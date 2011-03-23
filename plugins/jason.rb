84e8d2a7a41e12a946ac22829a7dacf47563a790
Kenny Hoxworth <hoxworth@gmail.com>
2011-03-23 09:53:40 -0400

    Adding weighted subreddits to !jason command to avoid a gonewild bomb 10% of the time.

--- plugins/jason.rb
+++ plugins/jason.rb
@@ -8,26 +8,46 @@ class Jason < Linkbot::Plugin
   
   def self.on_message(user, message, matches) 
     times = matches[0]
-        reddits = [
-          "/r/pics/",
-          "/r/comics/",
-          "/r/wallpaper/",
-          "/r/gonewild/",
-          "/r/funny/",
-          "/r/fffffffuuuuuuuuuuuu/",
-          "/r/reddit.com/",
-        	"/r/WTF/",
-        	"/r/bestof/",
-        	"/r/videos/",
-        	"/r/aww",
-        ]
+        reddits = {
+          "/r/pics/" => 10,
+          "/r/comics/" => 10,
+          "/r/wallpaper/" => 10,
+          "/r/gonewild/" => 2,
+          "/r/funny/" => 10,
+          "/r/fffffffuuuuuuuuuuuu/" => 10,
+          "/r/reddit.com/" => 10,
+        	"/r/WTF/" => 10,
+        	"/r/bestof/" => 10,
+        	"/r/videos/" => 10,
+        	"/r/aww" => 10,
+        }
+ 
+        # Build out a weighted range
+        total_value = reddits.values.reduce(:+)
+        last_end = 0.0
+        
+        reddits.each do |k,v|
+          reddits[k] = [last_end, (v.to_f / total_value)*100]
+          last_end += (v.to_f / total_value) * 100
+        end
+        
         times = times ? times.to_i : 1
         times = times <= 5 ? times : 5 
  
         messages = []
         
         1.upto(times) do
-          subreddit = "http://reddit.com#{reddits[rand(reddits.length)]}.json"
+          
+          # Brute force this mother
+          subreddit = nil
+          val = rand(100000)/1000.0
+          reddits.each {|k,v|
+            if val >= v[0] && val <= v[1]
+              subreddit = "http://reddit.com#{k}.json"
+              break
+            end
+          }
+          
           puts subreddit
           doc = ActiveSupport::JSON.decode(open(subreddit, "Cookie" => "reddit_session=8390507%2C2011-03-22T07%3A06%3A44%2C2516dcc69a22ad297b9900cbde147b365203bbbb").read)
           
