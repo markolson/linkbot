e807bc4344b59780a12c06ba8d6d8effa6270458
Jason Denney <jdenney@lgscout.com>
2011-04-01 17:21:55 -0400

    Added check to reduce imgur dupes

--- plugins/imgur.rb
+++ plugins/imgur.rb
@@ -13,8 +13,30 @@ class Imgur < Linkbot::Plugin
       doc = Hpricot(open("http://imgur.com/gallery?p=#{x}").read)
       imgs += doc.search("div[@class=big-preview] a img").collect{|m| m.attributes["src"].gsub("b.jpg", ".jpg")}
     end
+    
+    #reduce dupes
+    img = ""
+    attempts = 0
+    while unique_link == false do
+      img = imgs[rand(imgs.length)]
+      if link_unique?(img)
+        #add img to db
+        p "inserted img to db"
+        #Linkbot.db.execute("insert into links (user_id, dt, url) VALUES ('0', '#{Time.now.getgm.to_i}', '#{img}')") ) 
+        unique_link = true
+      end
 
-    [imgs[rand(imgs.length)]]
+      attempts += 1
+      
+      #give up after 20 attempts
+      break if attempts > 20
+    end
+
+    [img]
+  end
+ 
+  def link_unique?(link)
+      Linkbot.db.execute("select url from links where url == '#{link}'").nil?
   end
   
   def self.help
