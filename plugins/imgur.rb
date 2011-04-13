1ba8bdb1771c62ccf58fbb6d5e359f2cf01e26bf
bill <bill.mill@gmail.com>
2011-04-12 23:41:48 -0400

    imgur changed their HTML

--- plugins/imgur.rb
+++ plugins/imgur.rb
@@ -9,37 +9,24 @@ class Imgur < Linkbot::Plugin
   
   def self.on_message(user, message, matches) 
     imgs = []
+    puts "loading images"
     1.upto(3) do |x|
       doc = Hpricot(open("http://imgur.com/gallery?p=#{x}").read)
-      imgs += doc.search("div[@class=big-preview] a img").collect{|m| m.attributes["src"].gsub("b.jpg", ".jpg")}
-    end
-    
-    #reduce dupes
-    img = ""
-    attempts = 0
-    while unique_link == false do
-      img = imgs[rand(imgs.length)]
-      if link_unique?(img)
-        #add img to db
-        p "inserted img to db"
-        #Linkbot.db.execute("insert into links (user_id, dt, url) VALUES ('0', '#{Time.now.getgm.to_i}', '#{img}')") ) 
-        unique_link = true
+      puts doc
+      imgs += doc.search("div[@class=post] a img").collect do |m|
+        puts m
+        m.attributes["src"].gsub("b.jpg", ".jpg")
       end
-
-      attempts += 1
-      
-      #give up after 20 attempts
-      break if attempts > 20
     end
+    img = imgs[rand(imgs.length)]
+
+    puts "returning #{img}, #{imgs}"
 
     [img]
-  end
- 
-  def link_unique?(link)
-      Linkbot.db.execute("select url from links where url == '#{link}'").nil?
   end
   
   def self.help
     "!imgur - return a random top picture"
   end
 end
+
