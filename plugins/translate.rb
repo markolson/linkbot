869705aa9d62873ab156c248e62c6e028418bbdb
Kenny Hoxworth <hoxworth@gmail.com>
2011-04-23 14:38:49 -0400

    THIS IS WHY.

---
+++ plugins/translate.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'uri'
+require 'cgi'
+
+class Translate < Linkbot::Plugin
+    def self.on_message(user, message, matches, msg)
+      res = Net::HTTP.post_form(URI.parse('http://www.cs.utexas.edu/users/jbc/bork/bork.cgi'),
+                                 {'input'=> CGI.unescapeHTML(message_history[1]), 'type'=>'chef'})
+      return [CGI::unescape(res.body.to_s.gsub("&epus;","'"))]
+    end
+    
+    def self.help
+      "!translate - Trunslete-a zee lest messege-a"
+    end
+    
+    Linkbot::Plugin.register('translate', self,
+      {
+        :message => {:regex => /!translate/, :handler => :on_message, :help => :help}
+      }
+    )
+end
