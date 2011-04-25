3b86176d3b5ecd89c9aa840758c2acebf3958486
Kenny Hoxworth <hoxworth@gmail.com>
2011-04-25 10:10:31 -0400

    Modularized the titles plugin, modified the stores plugin to just send store messages to store plugins.

--- plugins/store.rb
+++ plugins/store.rb
@@ -5,51 +5,49 @@ class Store < Linkbot::Plugin
       if matches[0] && matches[0].length > 0
         args = matches[0].split(" ")
         command = args.shift
-        mymsg = case command
-        when "name"
-          cost = 50
-          rmsg = nil
-          if args.length > 1
-            target_user = args.shift
-            rows = Linkbot.db.execute("select username,showname from users where username='#{target_user}'")
-            if rows.length == 0
-              rmsg = "Unknown user"
+        
+        Linkbot::Plugin.plugins.each {|k,v|
+          if v[:handlers][:"linkbot-store"] && v[:handlers][:"linkbot-store"][:regex] && v[:handlers][:"linkbot-store"][:regex].match(command)
+            cost = v[:ptr].respond_to?(:cost) ? v[:ptr].send(:cost).to_i : 0
+            if check_karma(user, cost)
+              mymsg = v[:ptr].send(v[:handlers][:"linkbot-store"][:handler], user, args).join("\n")
+              deduct_karma(user, cost)
             else
-              old_username = (rows[0][1].nil? || rows[0][1] == '') ? rows[0][0] : rows[0][1]
-              new_username = args.join(" ")
-              if check_karma(cost, user)
-                Linkbot.db.execute("update users set showname='#{new_username}' where username='#{target_user}'")
-                rmsg = "'#{old_username}' is now known as '#{new_username}'"
-                deduct_karma(cost, user)
-              else
-                rmsg = "Not enough karma to do this."
-              end
+              mymsg = "Not enough karma"
             end
-          end
-          rmsg
-        else
-          nil
-        end
+          end 
+        }
       end
       
+      
       if mymsg.nil?
+        # Build out the menu
         mymsg = [
           "Karma Store",
           "-----------",
           "Buy some stuff with karma. To buy, just use the !store command, followed by the appropriate stuff.",
-          "",
-          "1. [COST 50] name (username) (new name): Change how a user's name appears in any linkbot command."
-        ].join("\n")
+          ""
+        ]
+        
+        num = 1
+        Linkbot::Plugin.plugins.each {|k,v|
+          if v[:handlers][:"linkbot-store"] && v[:ptr].respond_to?(:cost) && v[:ptr].respond_to?(:advertisement)
+            mymsg << "#{num}. [COST #{v[:ptr].send(:cost)}] #{v[:ptr].send(:advertisement)}"
+            num += 1
+          end 
+        }
+        
+        mymsg = mymsg.join("\n")
       end
       [mymsg]
     end
     
-    def self.check_karma(cost, user)
+    def self.check_karma(user, cost)
       karma = Linkbot.db.execute("select karma from karma where user_id=#{user['id']}")[0][0]
       karma > cost
     end
     
-    def self.deduct_karma(cost, user)
+    def self.deduct_karma(user, cost)
       karma = Linkbot.db.execute("select karma from karma where user_id=#{user['id']}")[0][0] - cost
       Linkbot.db.execute("update karma set karma=#{karma} where user_id=#{user['id']}")
     end
@@ -60,7 +58,7 @@ class Store < Linkbot::Plugin
     
     Linkbot::Plugin.register('store', self,
       {
-        :"direct-message" => {:regex => /!store(.*)/, :handler => :on_message, :help => :help}
+        :"direct-message" => {:regex => /!store(.*)/, :handler => :on_message, :help => :help},
       }
     )
 end
