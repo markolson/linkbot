50adab24411668b4638e86f80d4a7e4cecad913e
mark olson <theothermarkolson@gmail.com>
2011-04-28 15:17:34 -0400

    twss gem

--- plugins/twss.rb
+++ plugins/twss.rb
@@ -1,12 +1,20 @@
+require 'twss'
+
 class Twss < Linkbot::Plugin
     def self.on_message(user, message, matches, msg)
-      ["That's what she said"]
+      TWSS.threshold = 1.5 #lower = 'better'
+      if TWSS(message)
+        ["That's what she said"]
+      else
+        []
+      end
     end
-    
-    Linkbot::Plugin.register('what she said', self,
+
+    Linkbot::Plugin.register('twss', self,
       {
-        :message => {:regex => Regexp.new('more wood|push it in|break it off|inside my mouth|into my eyes|won\'t come out|i can\'t stop|in all the way|one this big|suck on it|guys on me|try the other hole|too short|do the big one|with your ball|just do it|make it squirt|sucking so hard|nothing comes out|suck so hard|it was hung|sucks hard|COMING IN MY MOUTH', 'i'),
-                     :handler => :on_message}
+        :message => {:handler => :on_message}
       }
     )
 end
+
+
