95f4ca2d8ff3eea7d463fb4546cfea7492108ed1
bill <bill.mill@gmail.com>
2011-05-09 14:53:05 -0400

    adding an onion plugin

---
+++ plugins/onion.rb
@@ -0,0 +1,19 @@
+require 'active_support'
+
+class Onion < Linkbot::Plugin
+  Linkbot::Plugin.register('onion', self,
+    {
+      :message => {:regex => Regexp.new('!onion'), :handler => :on_message, :help => :help},
+      :"direct-message" => {:regex => Regexp.new('!onion'), :handler => :on_message, :help => :help}
+    }
+  )
+  
+  def self.on_message(user, message, matches, msg) 
+    links = Hpricot(open('http://feeds.theonion.com/theonion/daily')).search('feedburner:origlink').collect{|l| l.html}
+    [links[rand(links.length)]]
+  end
+  
+  def self.help
+    "!onion - return an onion link from the RSS feed"
+  end
+end
