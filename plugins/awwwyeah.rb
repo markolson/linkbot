73f75f80de8da3fd7605e95b8c78cac674f1de63
Kenny Hoxworth <hoxworth@gmail.com>
2011-06-13 12:48:53 -0400

    Phonetic AWW YEAH

--- plugins/awwwyeah.rb
+++ plugins/awwwyeah.rb
@@ -1,11 +1,19 @@
+require 'rubyfish'
+
 class Awwyeah < Linkbot::Plugin
     def self.on_message(user, message, matches, msg)
-      ["http://i.imgur.com/Y3Q0Z.png"]
+      m = []
+      if RubyFish::LongestSubsequence.distance("aw yeah", message.downcase) == 7
+        m = ["http://i.imgur.com/Y3Q0Z.png"]
+      elsif  RubyFish::Jaro.distance("aw yeah", message.downcase) > 0.79
+        m = ["http://i.imgur.com/Y3Q0Z.png"]
+      end
+      m
     end
 
     Linkbot::Plugin.register('awwwyeah', self,
       {
-        :message => {:regex => /a+w+ y+e+a+h+/i, :handler => :on_message}
+        :message => {:regex => //, :handler => :on_message}
       }
     )
 end
