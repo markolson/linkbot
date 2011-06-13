bc25a37aaf4126e18ebb6070cfb0cd3921d7fab2
Kenny Hoxworth <hoxworth@gmail.com>
2011-06-13 12:57:39 -0400

    Okay okay, experiment over. I lose.

--- plugins/awwwyeah.rb
+++ plugins/awwwyeah.rb
@@ -1,19 +1,11 @@
-require 'rubyfish'
-
 class Awwyeah < Linkbot::Plugin
     def self.on_message(user, message, matches, msg)
-      m = []
-      if RubyFish::LongestSubsequence.distance("aw yeah", message.downcase) == 7
-        m = ["http://i.imgur.com/Y3Q0Z.png"]
-      elsif  RubyFish::Jaro.distance("aw yeah", message.downcase) > 0.79
-        m = ["http://i.imgur.com/Y3Q0Z.png"]
-      end
-      m
+      ["http://i.imgur.com/Y3Q0Z.png"]
     end
 
     Linkbot::Plugin.register('awwwyeah', self,
       {
-        :message => {:regex => //, :handler => :on_message}
+        :message => {:regex => /a+w+ y+e+a+h+/i, :handler => :on_message}
       }
     )
 end
