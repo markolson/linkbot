498de48696b2f35ce03268d5e1c57df2727c8172
bill <bill.mill@gmail.com>
2011-06-15 14:51:05 -0400

    adding okay plugin

---
+++ plugins/okay.rb
@@ -0,0 +1,11 @@
+class Okay < Linkbot::Plugin
+    def self.on_message(user, message, matches, msg)
+      ["http://i.imgur.com/p7uaa.jpg"]
+    end
+
+    Linkbot::Plugin.register('okay', self,
+      {
+        :message => {:regex => /okay\./i, :handler => :on_message}
+      }
+    )
+end
