5b44e6ef91fa2d9c6d371fc6311531aee5f2483a
bill <bill.mill@gmail.com>
2011-10-17 16:18:16 -0400

    I should really commit more often. First cut at a working linkbot... plugin interface is next.

--- linkbot.rb
+++ linkbot.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
+
+#gem install twitter-stream -s http://gemcutter.org
+require 'twitter/json_stream'
+
 require 'httparty'
+
 require 'sanitize'
 require 'htmlentities'
 
@@ -13,54 +18,55 @@ require 'base_plugins'
 
 class Linkbot
   include HTTParty
-  base_uri 'convore.com:443/api'
+  base_uri BASE_URI
   basic_auth USER, PASS
+  headers 'Content-Type' => 'application/json' 
+  debug_output $stderr
 
-  def self.live
-    cursor = nil
-    while true
-      begin
-        Linkbot::Plugin.collect
+  def initialize
+    Linkbot::Plugin.collect
+    #{"user":{"type":"Member",
+    #         "created_at":"2011/10/17 18:14:21 +0000",
+    #         "avatar_url":"http://asset0.37img.com/global/missing/avatar.png?r=3",
+    #         "id":1031898,
+    #         "admin":false,
+    #         "name":"Linkbot McGee",
+    #         "email_address":"bill.mill+linkbot@gmail.com",
+    #         "api_auth_token":"db8b174571365a8e638528f8eca5e43ca6860b26"}}
+    @my_user = JSON.load(self.class.get("/users/me.json").body)["user"]
+    self.class.basic_auth @my_user["api_auth_token"], "x"
+    
+    joinroom = "/room/#{ROOM_ID}/join.xml"
+    response = self.class.post(joinroom, :body => "_")
 
-        options = cursor.nil? ? {} : {'cursor' => cursor}
-        puts "requesting"
-        results = get('/live.json', options)
-
-        messages = JSON.load(results.body)['messages']
-        
-        messages.each do |m|
-          cursor = m["_id"]
-
-          # if it wasn't sent by us, continue
-          user = m['user']
-          
-          next if user['username'] == USER
-
-          #ignore some verbose messages
-          if !(['login', 'logout', 'read'].include? m['kind'])
-            puts m
-          end
-          
-          # Create the user's info, if it does not exist
-          rows = Linkbot.db.execute("select * from users where user_id = '#{user['id']}'")
-          if rows.empty?
-            Linkbot.db.execute("insert into users (user_id,username) values ('#{user['id']}', '#{user['username']}')")
-          end
-
-          # Handle the message
-          Linkbot::Plugin.handle_message(user, m)
-        end
-
-      #unless it's an interrupt (i.e. ^C),
-      rescue Interrupt
-        exit
-      #catch it and keep on truckin
-      rescue Exception
-        #TODO: just put that exception
-        puts $!
-        puts $!.backtrace.join "\n"
-      end
+    if response.response.code != "200"
+      raise "Unable to join room #{joinroom}, got #{response.response}"
     end
+  end
+
+  def my_user
+    @my_user
+  end
+
+  def process(message)
+    message = JSON.parse(message)
+    return if message["type"] != "TextMessage"
+
+    #DELETEME
+    pp message
+    
+    # if it wasn't sent by us, continue
+    return if message['user_id'] == @my_user["id"]
+
+    # Create the user's info, if it does not exist
+    rows = Linkbot.db.execute("select * from users where user_id = '#{message['user_id']}'")
+    if rows.empty?
+      user = JSON.parse(self.class.get("/users/#{message['user_id']}.json").body)["user"]
+      Linkbot.db.execute("insert into users (user_id,username) values ('#{user['id']}', '#{user['name']}')")
+    end
+
+    # Handle the message
+    Linkbot::Plugin.handle_message(user, m)
   end
 
   def self.msg(url, msg)
@@ -78,4 +84,27 @@ class Linkbot
   end
 end
 
-Linkbot.live
+linkbot = Linkbot.new
+
+options = {
+  :path => "/room/#{ROOM_ID}/live.json",
+  :host => "streaming.campfirenow.com",
+  :auth => "#{linkbot.my_user['api_auth_token']}:x"
+}
+
+EventMachine::run do
+  stream = Twitter::JSONStream.connect(options)
+
+  stream.each_item do |item|
+    linkbot.process item
+  end
+ 
+  stream.on_error do |message|
+    puts "ERROR:#{message.inspect}"
+  end
+ 
+  stream.on_max_reconnects do |timeout, retries|
+    puts "Tried #{retries} times to connect."
+    exit
+  end
+end
