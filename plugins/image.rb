c9caab14ba23894ed7c5f17515eaa32a7ccf55f0
Kenny Hoxworth <hoxworth@gmail.com>
2011-10-24 11:22:41 -0400

    Adding google image plugin

---
+++ plugins/image.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'uri'
+
+class Image < Linkbot::Plugin
+  
+  Linkbot::Plugin.register('image', self,
+    {
+      :message => {:regex => /!image(?: (\w+))?/, :handler => :on_message, :help => :help}
+    }
+  )
+  
+  def self.on_message(message, matches, msg) 
+    searchterm = matches[0]
+    if searchterm.nil?
+      doc = Hpricot(open("http://www.randomword.net").read)
+      searchterm = doc.search("#word h2").text.strip
+    end
+    doc = JSON.parse(open("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{URI.encode(searchterm)}&rsz=8&safe=off", "Referer" => "http://lgscout.com").read)
+    if doc["responseData"]["results"].length > 0
+      doc["responseData"]["results"][rand(doc["responseData"]["results"].length)]["url"]
+    else
+      "No pictures found! Nuts!"
+    end
+  end
+  
+  def self.help
+    "!image [searchity search] - Return a relevant picture"
+  end
+end
+
