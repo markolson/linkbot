a22cac6b819a115c9d3b0e25680c7be9e8a5d456
Kenny Hoxworth <hoxworth@gmail.com>
2011-11-02 14:17:53 -0400

    Add video player

---
+++ plugins/video.rb
@@ -0,0 +1,33 @@
+class Video < Linkbot::Plugin
+  include HTTParty
+  
+  Linkbot::Plugin.register('video', self, {
+    :message => {:regex => /^!video (.+)/i, :handler => :on_message}
+  })
+
+  def self.on_message(message, matches)
+    default_videos = {
+      "bear" => "FiARsQSlzDc",
+      "das" => "SIt2CdbBo_w",
+      "erlang" => "uKfKtXYLG78",
+      "pizza" => "wusGIl3v044"
+    }
+    
+    url = ""
+    key = matches[0].strip
+    if key.downcase == 'stop'
+      url = "#{Linkbot::Config["plugins"]["video"]["base_uri"]}/youtube/stop"
+    elsif default_videos.has_key?(key)
+      url = "#{Linkbot::Config["plugins"]["video"]["base_uri"]}/youtube/#{default_videos[key]}"
+    else
+      url = "#{Linkbot::Config["plugins"]["video"]["base_uri"]}/youtube/#{key}"
+    end
+    
+    get(url)
+    ''
+  end
+  
+  def self.help
+    "!video (youtube video | stop) - play a video, or stop a video. Or call a built-in video. Or something."
+  end
+end
