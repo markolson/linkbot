c90d7d5073aa232a3edce29a64b8c447b3305457
Kenny Hoxworth <hoxworth@gmail.com>
2011-11-04 08:37:42 -0400

    Adding fail plugin (this did not exist? WTF?)

---
+++ plugins/fail.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+
+class Fail < Linkbot::Plugin
+  include HTTParty
+    
+  Linkbot::Plugin.register('fail', self, {
+    :message => {:regex => /^fail( .+)?/i, :handler => :on_message}
+  })
+
+  def self.on_message(message, matches)
+    sound = true
+    doc = JSON.parse(open("http://www.failpictures.com/index.php?module=ajax&action=ajax_init&cpaint_function=getimg&cpaint_response_type=JSON").read)
+    
+    if Linkbot::Config["plugins"]["fail"]["webhook"]
+      get("#{Linkbot::Config["plugins"]["fail"]["webhook"]}")
+    end
+    
+    doc["code"][0]["data"]
+  end
+
+end
