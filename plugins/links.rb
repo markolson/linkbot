156ac5b41f8956efa237e59da1311eae8982417e
Kenny Hoxworth <hoxworth@gmail.com>
2012-01-24 10:36:59 -0500

    Yeah who cut and pasted?

--- plugins/links.rb
+++ plugins/links.rb
@@ -1,4 +1,4 @@
-class Awwyeah < Linkbot::Plugin
+class Links < Linkbot::Plugin
     Linkbot::Plugin.register('links', self,
     {
       :message => {:regex => Regexp.new('(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'), 
