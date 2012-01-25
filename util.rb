a3e606a81a66003a61dd7e07cd683d7c8bd4ff32
Kenny Hoxworth <hoxworth@gmail.com>
2012-01-25 11:22:36 -0500

    Make the time output look a little better than something non-sensical

--- util.rb
+++ util.rb
@@ -1,6 +1,7 @@
 module Util
   def self.ago_in_words(time1, time2)
     diff = time1.to_i - time2.to_i
+
     ago = ''
     if diff == 1
       ago = "#{diff} second ago"
@@ -9,19 +10,19 @@ module Util
     elsif diff < 120
       ago = "a minute ago"
     elsif diff < 3600
-      ago = "#{diff.to_i / 60} minutes ago"
+      ago = "#{(diff / 60).to_i} minutes ago"
     elsif diff < 7200
       ago = "an hour ago"
     elsif diff < 86400
-      ago = "#{diff.to_i / 3600} hours ago"
+      ago = "#{(diff / 3600).to_i} hours ago"
     elsif diff < 172800
       ago = "yesterday"
     elsif diff < 604800
-      ago = "#{diff.to_i / 86400} days ago"
+      ago = "#{(diff / 86400).to_i} days ago"
     elsif diff < 1209600
       ago = "last week"
     else
-      ago = "#{diff.to_i / 604800} weeks ago"
+      ago = "#{(diff / 604800).to_i} weeks ago"
     end
     ago
   end
