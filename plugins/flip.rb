dcb569d379b9732cb652a7fe123d298429bb42f1
bill <bill.mill@gmail.com>
2012-03-01 16:38:26 -0500

    fixing the first branch

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -10,7 +10,7 @@ class Flip < Linkbot::Plugin
     if not matches or not matches[0]
       return rand(2) == 0 ? "heads" : "tails"
     elsif matches[0].match /\d+/
-      return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }
+      return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }.join ", "
     else
       return matches[1].split(',').sort_by { rand }.map { |x| x.strip }
     end
