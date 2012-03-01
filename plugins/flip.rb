69cecdd12c1306e3f705dd1041c61dc1b54c48c4
bill <bill.mill@gmail.com>
2012-03-01 16:20:46 -0500

    fix fix fix

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -1,7 +1,7 @@
 class Flip < Linkbot::Plugin
   Linkbot::Plugin.register('jason', self,
     {
-      :message => {:regex => Regexp.new('!flip(?: (\d+)|([\w\s,]+))?'), :handler => :on_message, :help => :help},
+      :message => {:regex => Regexp.new('!flip(?: (\d+)|([\d\w\s,]+))?'), :handler => :on_message, :help => :help},
       :"direct-message" => {:regex => Regexp.new('!flip(?: (\d+)|([\w\s,]+))?'), :handler => :on_message, :help => :help}
     }
   )
@@ -10,9 +10,9 @@ class Flip < Linkbot::Plugin
     if not matches or not matches[0]
       return rand(2) == 0 ? "heads" : "tails"
     elsif matches[0].match /\d+/
-      return (1..matches[1].to_i).map { rand(2) == 0 ? "heads" : "tails" }
+      return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }
     else
-      return matches[2].split(',').sort_by { rand }.map { |x| x.strip }
+      return matches[1].split(',').sort_by { rand }.map { |x| x.strip }
     end
   end
   
