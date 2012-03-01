eea7d5def9cc9282b845865df6c3e7aa6cdb823e
bill <bill.mill@gmail.com>
2012-03-01 16:11:57 -0500

    fix fix fix

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -10,9 +10,9 @@ class Flip < Linkbot::Plugin
     if not matches or not matches[0]
       return rand(2) == 0 ? "heads" : "tails"
     elsif matches[0].match /\d+/
-      return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }
+      return (1..matches[1].to_i).map { rand(2) == 0 ? "heads" : "tails" }
     else
-      return matches[0].split(',').sort_by { rand }
+      return matches[2].split(',').sort_by { rand }.map { |x| x.strip }
     end
   end
   
