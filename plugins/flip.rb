1b6087600fa2f79b22e5151017db5a9f77d53cf5
bill <bill.mill@gmail.com>
2012-03-01 15:19:40 -0500

    fix fix fix

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -7,7 +7,7 @@ class Flip < Linkbot::Plugin
   )
   
   def self.on_message(message, matches) 
-    if not matches
+    if not matches or not matches[0]
       return rand(2) == 0 ? "heads" : "tails"
     elsif matches[0].match /\d+/
       return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }
