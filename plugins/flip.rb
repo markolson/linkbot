74606c9e7c8097ee810ddf2fb2df41f897819277
bill <bill.mill@gmail.com>
2012-03-01 16:55:33 -0500

    really for real fix flip

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -5,14 +5,14 @@ class Flip < Linkbot::Plugin
       :"direct-message" => {:regex => Regexp.new('!flip(?: (\d+)|([\w\s,]+))?'), :handler => :on_message, :help => :help}
     }
   )
-  
+
   def self.on_message(message, matches) 
-    if not matches or not matches[0]
+    if !matches || matches == [nil, nil]
       return rand(2) == 0 ? "heads" : "tails"
-    elsif matches[0].match /\d+/
+    elsif matches[0] && matches[0].match(/\d+/)
       return (1..matches[0].to_i).map { rand(2) == 0 ? "heads" : "tails" }.join ", "
     else
-      return matches[1].split(',').sort_by { rand }.map { |x| x.strip }
+      return matches[1].split(',').sort_by { rand }.map { |x| x.strip }.join ", "
     end
   end
   
