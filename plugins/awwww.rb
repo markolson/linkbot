65dc2b8a9234db1b9c3f2eed210b64afcd5ef59b
bill <bill.mill@gmail.com>
2012-03-08 12:12:14 -0500

    a better awwww.rb

--- plugins/awwww.rb
+++ plugins/awwww.rb
@@ -1,11 +1,23 @@
 class Awwww < Linkbot::Plugin
-    def self.on_message(message, matches)
-      "http://i.imgur.com/DALJo.jpg"
-    end
-
     Linkbot::Plugin.register('awwww', self,
       {
         :message => {:regex => /^a+w+$/i, :handler => :on_message}
       }
     )
+
+    def self.on_message(message, matches)
+      reddit = "http://www.reddit.com/r/aww.json"
+      doc = ActiveSupport::JSON.decode(open(reddit).read)
+      url = doc["data"]["children"][rand(doc["data"]["children"].length)]["data"]["url"]
+      
+      # Check if it's an imgur link without an image extension
+      if url =~ /http:\/\/(www\.)?imgur\.com/ && !['jpg','png','gif'].include?(url.split('.').last)
+        # Fetch the imgur page and pull out the image
+        doc = Hpricot(open(url).read)
+        url = doc.search("img")[1]['src']
+      end
+      
+      return url
+    end
+
 end
