66fce67d4fc414817c9467a027ba768da372b378
Kenny Hoxworth <hoxworth@gmail.com>
2012-03-15 14:26:43 -0400

    Linux!

--- plugins/image.rb
+++ plugins/image.rb
@@ -1,8 +1,29 @@
 require 'open-uri'
+require 'image_size'
 require 'uri'
 require 'cgi'
 
 class Image < Linkbot::Plugin
+  @@resolutions = {
+    "800x600" => true,
+    "1024x600" => true,
+    "1024x768" => true,
+    "1152x864" => true,
+    "1280x720" => true,
+    "1280x768" => true,
+    "1280x800" => true,
+    "1280x960" => true,
+    "1280x1024" => true,
+    "1360x768" => true,
+    "1366x768" => true,
+    "1440x900" => true,
+    "1600x900" => true,
+    "1600x1200" => true,
+    "1680x1050" => true,
+    "1920x1080" => true,
+    "1920x1200" => true,
+    "2560x1440" => true
+  }
   
   Linkbot::Plugin.register('image', self,
     {
@@ -27,7 +48,17 @@ class Image < Linkbot::Plugin
     end
     doc = JSON.parse(open("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{URI.encode(searchterm)}&rsz=8&safe=off#{color.nil? ? '' : '&imcolor=' + color}", "Referer" => "http://lgscout.com").read)
     if doc["responseData"]["results"].length > 0
-      URI.decode(doc["responseData"]["results"][rand(doc["responseData"]["results"].length)]["url"])
+      url = URI.decode(doc["responseData"]["results"][rand(doc["responseData"]["results"].length)]["url"])
+      
+      d = nil
+      open(url) do |fh|
+        d = ImageSize.new(fh.read).get_size
+      end
+      
+      if @@resolutions.has_key?("#{d[0]}x#{d[1]}")
+        url = "#{url}\nWALLPAPER WALLPAPER WALLPAPER WALLPAPER WALLPAPER"
+      end
+      url
     else
       "No pictures found! Nuts!"
     end
