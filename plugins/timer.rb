fd2d9376c022df9bf45207a6f255fa82dda32e6e
Kenny Hoxworth <hoxworth@gmail.com>
2012-04-18 14:50:52 -0400

    I should have known to trust you dolts.

--- plugins/timer.rb
+++ plugins/timer.rb
@@ -1,16 +1,25 @@
 class Timer < Linkbot::Plugin
 
+  @@active = 0
+
   def self.help
-    "!timer <seconds> - set a timer"
+    "!timer <seconds> [message] - set a timer, optionally saying something."
   end
 
   def self.on_message(message, matches)
-    seconds = matches[0].to_i
-    sleep(seconds)
-    "NURRRRRRRR ALARM NURRRRRRR"
+    if @@active >= 10
+      "There are currently 10 timers running. Wait your turn."
+    elsif matches[0].to_i > 300
+      "Timers cannot be longer than 5 minutes"
+    else
+      seconds = matches[0].to_i
+      message = matches[1].length > 0 ? matches[1] : "NURRRRRR ALARM NURRRRR"
+      sleep(seconds)
+      message
+    end
   end
 
   Linkbot::Plugin.register('timer', self, {
-    :message => {:regex => /!timer (\d+)/i, :handler => :on_message, :help => :help}
+    :message => {:regex => /!timer (\d+)\s?(.*)/i, :handler => :on_message, :help => :help}
   })
 end
