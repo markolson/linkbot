9f6842e609e9997164955ff0cde35dde718b4bf5
bill <bill.mill@gmail.com>
2012-05-16 16:21:40 -0400

    update pull requests for new periodic message style

--- plugins/pull_requests.rb
+++ plugins/pull_requests.rb
@@ -19,29 +19,29 @@ class PullRequests < Linkbot::Plugin
     )
   end
 
-  def self.periodic()
-    min_pull = 0
-    rows = Linkbot.db.execute("select max(number) from pull_requests")
-    min_pull = rows[0][0].to_i if !rows.empty?
-    
-    res = get("/repos/#{@@config["owner"]}/#{@@config["project"]}/pulls")
-    response = res.body
-    JSON.load(response)
-    if res.code >= 200 && res.code < 300
-      messages = []
-      response.each do |pullreq|
-        next if pullreq["number"].to_i <= min_pull
-        messages << "New pull request:\n#{pullreq['title']}\n#{pullreq['body']}\n#{pullreq['url']}"
-        Linkbot.db.execute("insert into pull_requests (number) VALUES (#{pullreq["number"]})")
-      end
-      pp "returning #{messages}"
-      messages
-    else
-      puts "Login error"
-    end
-  end
-  
   if @@config
+    def self.periodic()
+      min_pull = 0
+      rows = Linkbot.db.execute("select max(number) from pull_requests")
+      min_pull = rows[0][0].to_i if !rows.empty?
+
+      res = get("/repos/#{@@config["owner"]}/#{@@config["project"]}/pulls")
+      response = res.body
+      JSON.load(response)
+      if res.code >= 200 && res.code < 300
+        messages = []
+        response.each do |pullreq|
+          next if pullreq["number"].to_i <= min_pull
+          messages << "New pull request:\n#{pullreq['title']}\n#{pullreq['body']}\n#{pullreq['url']}"
+          Linkbot.db.execute("insert into pull_requests (number) VALUES (#{pullreq["number"]})")
+        end
+
+        {:messages => messages, :options => {:room => @@config[:room]}}
+      else
+        puts "Login error"
+      end
+    end
+
     if Linkbot.db.table_info('pull_requests').empty?
       Linkbot.db.execute('CREATE TABLE pull_requests (number INTEGER)');
     end
