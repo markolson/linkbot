7240b18a4f32d0bd147efc9cf99cddcef432a9b1
bill <bill.mill@gmail.com>
2012-06-11 10:55:37 -0400

    updating toothpaste to the new message format

--- plugins/toothpaste.rb
+++ plugins/toothpaste.rb
@@ -9,7 +9,7 @@ class Toothpaste < Linkbot::Plugin
         doc = Hpricot(open('http://www.toothpastefordinner.com/randomComicViewer.php'))
         img = doc.search('#comicarea img')
         link = doc.search('div.headertext a:first').text()
-        "#{link.strip}\n#{img.first['src']}"
+        [link.strip, img.first['src']]
     end
 end
 
