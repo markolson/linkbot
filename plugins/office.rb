6956c9661ff92160cddf376452960bbe42342205
Kenny Hoxworth <hoxworth@gmail.com>
2012-06-12 10:32:35 -0400

    And somehow THAT got in there

--- plugins/office.rb
+++ plugins/office.rb
@@ -15,7 +15,7 @@ class Office < Linkbot::Plugin
       Linkbot.db.execute("update presence set user_id='#{message.user_id}' where ip='#{$1}'")
     else
       if matches[0] && matches[0].length > 0
-        users = Linkbot.db.execute("select u.username,max(p.present),max(p.last_seen) from users u,presence p where u.user_id=p.user_id AND p.present = 0 AND u.username LIKE '%#{matches[0]}%' GROUP BY u.username ORDER BY p.present DESC")
+        users = Linkbot.db.execute("select u.username,max(p.present),max(p.last_seen) from users u,presence p where u.user_id=p.user_id AND u.username LIKE '%#{matches[0]}%' GROUP BY u.username ORDER BY p.present DESC")
         messages = users.map {|e| 
           if e[1].to_i == 0
             "#{e[0]} (out of office, last seen #{Time.at(e[2].to_i)})"
