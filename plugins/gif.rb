ffa92295af28c53b7c41b01ca9bf6cef6907197a
Bill Mill <bill.mill@gmail.com>
2012-07-23 16:23:25 -0400

    No need to check gifs for wallpaperness

--- plugins/gif.rb
+++ plugins/gif.rb
@@ -29,10 +29,6 @@ class Gif < Linkbot::Plugin
       # Fetch the imgur page and pull out the image
       doc = Hpricot(open(url).read)
       url = doc.search("img")[1]['src']
-
-      if ::Util.wallpaper?(url)
-        url = [url, "(dealwithit) WALLPAPER WALLPAPER WALLPAPER (dealwithit)"]
-      end
     end
 
     url
