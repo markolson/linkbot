6ea6a42e71a3b702a1cf4301cc0a6514b3e8f195
Bill Mill <bill.mill@gmail.com>
2012-12-18 16:02:30 -0500

    add roster to room messages and fix whitespace in jabber connector

--- connectors/jabber.rb
+++ connectors/jabber.rb
@@ -7,7 +7,7 @@ class JabberConnector < Linkbot::Connector
 
   def initialize(options)
     super(options)
-        
+
     @fullname = @options["fullname"] ? @options["fullname"] : @options["username"]
     @username = @options["username"]
     @password = @options["password"]
@@ -20,12 +20,12 @@ class JabberConnector < Linkbot::Connector
 
     listen
   end
-  
+
   def update_users
     puts "Retrieving roster..."
     @roster.get_roster
     @roster.wait_for_roster
-    
+
     @roster.items.values.each do |roster_item|
       user_id = roster_item.jid.node
       if !Linkbot.user_exists?(user_id)
@@ -34,9 +34,9 @@ class JabberConnector < Linkbot::Connector
         Linkbot.update_user(roster_item.iname,user_id)
       end
     end
-    
+
   end
-  
+
   def reconnect
     puts "Connecting and authenticating..."
     @connection = ::Jabber::Client.new(Jabber::JID.new("#{@username}@#{@server}/#{@resource}"))
@@ -50,16 +50,16 @@ class JabberConnector < Linkbot::Connector
       sleep 5
       puts "Connection failed: #{exception} at #{sym}"
       puts "Reconnecting..."
-      reconnect 
+      reconnect
     end
-    
+
     @roster = Jabber::Roster::Helper.new(@connection)
     @roster.wait_for_roster
-  
+
     @connection.send(::Jabber::Presence.new.set_type(:available))
 
     update_users
-    
+
     puts "Adding callbacks..."
 
     @connection.add_message_callback do |m|
@@ -82,53 +82,53 @@ class JabberConnector < Linkbot::Connector
 
     @rooms.each do |room|
       muc = ::Jabber::MUC::SimpleMUCClient.new(@connection)
-  
+
       muc.add_message_callback do |m|
         if m.type.to_s == "groupchat" && m.body
           begin
             room = m.from.node
             nick = m.from.resource
             if nick != @options["fullname"]
-              process_message(Time.now,nick,m.body,{:room => room})
+              process_message(Time.now,nick,m.body,{:user => m.from, :roster => @roster})
             end
           rescue
             puts $!.message
           end
         end
       end
-      
+
       muc.join("#{room}@#{@conference}/#{@fullname}")
       @mucs[room] = muc
     end
   end
-  
+
   def process_message(time, nick, text, options={})
     # Attempt to get the user from the roster
-    
+
     if !Linkbot.user_exists?(nick)
       puts "Encountered new user '#{nick}' while processing message '#{text}'"
       update_users
     end
-    
+
     if Linkbot.user_exists?(nick)
-      message = Message.new( text, Linkbot.user_id(nick), nick, self, :message, options )  
+      message = Message.new( text, Linkbot.user_id(nick), nick, self, :message, options )
       invoke_callbacks(message, options)
     end
   end
-  
+
   def send_messages(messages, options={})
     messages.each do |message|
       if message && message.strip.length > 0
         if options[:room] && @mucs[options[:room]]
           @mucs[options[:room]].send(::Jabber::Message.new(nil,message),nil)
-        
+
           # I don't like this, but hipchat has problems receiving a lot of messages at once in-order and keeping them in order.
           sleep(1.0/10.0)
         elsif options[:user]
           msg = ::Jabber::Message.new(options[:user],message)
           msg.type = :chat
           @connection.send(msg)
-        
+
           # I don't like this, but hipchat has problems receiving a lot of messages at once in-order and keeping them in order.
           sleep(1.0/25.0)
         end
