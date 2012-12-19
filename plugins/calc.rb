6dbb363ee6dcc4ed1629cd612a285d1239a04ce7
Bill Mill <bill.mill@gmail.com>
2012-12-19 14:29:06 -0500

    send !calc messages via jabber, not the API

--- plugins/calc.rb
+++ plugins/calc.rb
@@ -2,38 +2,9 @@ require 'open-uri'
 require 'cgi'
 
 class Calc < Linkbot::Plugin
-  @@config = Linkbot::Config["plugins"]["hipchat"]
-
-  if @@config
-    Linkbot::Plugin.register('calc', self, {
-      :message => {:regex => /!calc (.+)/i, :handler => :on_message}
-    })
-  end
-
-  def self.api_send(room, message)
-    return if message.empty?
-
-    message = CGI.escape(message)
-    color = "gray"
-    from = "calc"
-    token = @@config['api_token']
-    raise "no api token" if !token
-    begin
-      url = "https://api.hipchat.com/v1/rooms/message?" \
-          + "auth_token=#{token}&" \
-          + "message_format=html&" \
-          + "color=#{color}&" \
-          + "room_id=#{room}&" \
-          + "from=#{from}&" \
-          + "message=#{message}"
-
-          puts "sending hipchat messaage to #{url}"
-      open(url)
-    rescue => e
-      puts e.inspect
-      puts e.backtrace.join("\n")
-    end
-  end
+  Linkbot::Plugin.register('calc', self, {
+    :message => {:regex => /!calc (.+)/i, :handler => :on_message}
+  })
 
   def self.on_message(message, matches)
     query = CGI.escape(matches[0])
@@ -50,9 +21,6 @@ class Calc < Linkbot::Plugin
     answer.gsub! /<sup>/, '^'
     answer.gsub! /<\/sup>/, ''
 
-    room = message[:options][:room] || "16485_link_bot_test_3"
-    room = room.split('_', 2)[1]
-    self.api_send room, answer
-    []
+    [answer]
   end
 end
