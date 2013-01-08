de68d6fb6960ef8171cad9036c5a58b857b9a633
Bill Mill <bill.mill@gmail.com>
2013-01-07 23:10:41 -0500

    improve slap for @jalewis

--- plugins/slap.rb
+++ plugins/slap.rb
@@ -4,11 +4,17 @@ class Slap < Linkbot::Plugin
       :message => {:regex => /\/slap(?: ([\w\s]+))?/, :handler => :on_message, :help => :help}
     }
   )
-  
+
   def self.on_message(message, matches)
-    "#{message.user_name} slaps #{matches[0]} around a bit with a large trout"
+    if matches[0] and matches[0].length > 0
+      user = matches[0]
+    else
+      user = "everyone"
+    end
+
+    "#{message.user_name} slaps #{user} around a bit with a large trout"
   end
-  
+
   def self.help
     "/slap [username] - Flashback to the halcyon days of the 1990s when hammer pants were all the rage"
   end
