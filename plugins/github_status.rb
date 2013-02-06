21b1bc20b1e3066281bd2e5ebfb165df9c56ab64
Robb Kidd <robb@thekidds.org>
2013-02-05 22:18:14 -0500

    GitHub Status overhaul: use HipChat API for colors, use instance methods.

--- plugins/github_status.rb
+++ plugins/github_status.rb
@@ -1,19 +1,71 @@
 require 'json'
 require 'open-uri'
 
-class Hubup < Linkbot::Plugin
-  Linkbot::Plugin.register('hubup', self, {
-   :message => { :regex => /\A!hubup/, :handler => :on_message }
+class GithubStatus < Linkbot::Plugin
+  @@hipchat = Linkbot::Config["plugins"]["hipchat"]
+
+  Linkbot::Plugin.register('github_status', self, {
+   :message => { :regex => /\A!hubstat/, :handler => :on_message, :help => :help }
   })
 
-  def self.on_message(message, matches)
-    last_message = get_status
-    "As of #{last_message['created_on']}, GitHub is <a href='https://status.github.com/'>reporting</a>: #{last_message['status'].upcase} - #{last_message['body']}"
+  def self.help
+    '!hubstat - see whether your trouble with GitHub is just you'
   end
 
-  def self.get_status
+  def self.on_message(message, matches)
     response = open('https://status.github.com/api/last-message.json').read
-    JSON.parse(response)
+    last_message = JSON.parse(response)
+    GithubStatus.new(last_message).notify
+  end
+
+  attr_reader :api_response
+
+  def initialize(api_response)
+    @api_response = api_response
+  end
+
+  def notify
+    hipchat_send("As of #{timestamp}, GitHub is <a href='https://status.github.com/'>reporting</a>: #{notice}")
+  end
+
+  private
+
+  def timestamp
+    api_response['created_on']
+  end
+
+  def notice
+    api_response['body']
+  end
+
+  def color
+    case api_response['status']
+    when 'good'
+      'green'
+    when 'minor'
+      'yellow'
+    when 'major'
+      'red'
+    else
+      'gray'
+    end
+  end
+
+  def hipchat_send(message)
+    message = CGI.escape(message)
+
+    url = "https://api.hipchat.com/v1/rooms/message?" \
+        + "auth_token=#{@@hipchat['api_token']}&" \
+        + "message=#{message}&" \
+        + "color=#{color}&" \
+        + "room_id=#{@@hipchat['room']}&" \
+        + "from=GitHub+Status"
+
+    puts "sending message to hipchat url #{url}"
+    open(url)
+  rescue => e
+    puts e.inspect
+    puts e.backtrace.join("\n")
   end
 
 end
