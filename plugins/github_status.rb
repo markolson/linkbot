f0f96e47e10d644225ff575f4062d3556656b2bc
Bill Mill <bill.mill@gmail.com>
2013-02-17 14:15:52 -0500

    add periodic handler to hub_status and handle times better

--- plugins/github_status.rb
+++ plugins/github_status.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'open-uri'
+require 'active_support/time'
 
 class Hubstat < Linkbot::Plugin
   @@config = Linkbot::Config["plugins"]["hubstat"]
@@ -7,15 +8,20 @@ class Hubstat < Linkbot::Plugin
 
   if @@config && @@hipchat
     Linkbot::Plugin.register('hubstat', self, {
-     :message => { :regex => /\A!hubstat/, :handler => :on_message, :help => :help }
+     :message => { :regex => /\A!hubstat/, :handler => :on_message, :help => :help },
+     :periodic => {:handler => :periodic}
     })
+  end
+
+  if Linkbot.db.table_info('hubstatus').empty?
+    Linkbot.db.execute('CREATE TABLE hubstatus (dt TEXT)');
   end
 
   def self.help
     '!hubstat - see whether your trouble with GitHub is just you'
   end
 
-  def self.on_message(message, matches)
+  def self.post_status(last_pulled)
     response = JSON.parse(open('https://status.github.com/api/last-message.json').read)
 
     colors = {
@@ -25,8 +31,33 @@ class Hubstat < Linkbot::Plugin
     }
     color = colors.fetch(response['status'], "gray")
 
-    message = "As of #{response["created_on"]}, GitHub is <a href='https://status.github.com/'>reporting</a>: #{response["body"]}"
+    #don't print the message if time exists and is newer than the message
+    message_time = Time.parse(response["created_on"])
+    return last_pulled if last_pulled && last_pulled > message_time
+
+    timestr = message_time.in_time_zone("EST").strftime("%b %d %H:%m EST")
+
+    message = "As of #{timestr}, GitHub is <a href='https://status.github.com/'>reporting</a>: #{response["body"]}"
     hipchat_send(color, message)
+  end
+
+  def self.periodic
+    #by default, post the message if it's within the last day
+    last_pulled = Time.now.utc - 60*60*24
+
+    rows = Linkbot.db.execute("select dt from hubstatus")
+    last_pulled = Time.parse(rows[0][0]) if !rows.empty? && rows[0][0]
+
+    max_time = self.post_status(last_pulled)
+
+    Linkbot.db.execute("delete from hubstatus")
+    Linkbot.db.execute("insert into hubstatus (dt) VALUES ('#{max_time}')")
+    {:messages => []}
+  end
+
+  def self.on_message(message, matches)
+    self.post_status(nil)
+    []
   end
 
   def self.hipchat_send(color, message)
