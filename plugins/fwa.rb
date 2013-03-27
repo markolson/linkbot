68bb30e80f8c4bac439384ed403fa0fba9491d64
Bill Mill <bill.mill@gmail.com>
2013-03-27 09:37:11 -0400

    adding a firstworldanarchists plugin

---
+++ plugins/fwa.rb
@@ -0,0 +1,39 @@
+require 'open-uri'
+require 'hpricot'
+
+class Fwa < Linkbot::Plugin
+
+  def self.help
+    "!fwa - get an image from /r/firstworldanarchists"
+  end
+
+  create_log(:images)
+
+  def self.on_message(message, matches)
+    url = "http://reddit.com/r/firstworldanarchists.json"
+
+    doc = ActiveSupport::JSON.decode(open(url).read)
+
+    #reject anything with nsfw in the title
+    doc = doc["data"]["children"].reject {|x| x["data"]["title"] =~ /nsfw/i || x["data"]["over_18"]}
+
+    return "XXX FIAL XXX" if doc.empty?
+
+    randdoc = rand(doc.length)
+    url = doc[randdoc]["data"]["url"]
+    title = doc[randdoc]["data"]["title"]
+
+    # Check if it's an imgur link without an image extension
+    if url =~ /http:\/\/(www\.)?imgur\.com/ && !['jpg','png','gif'].include?(url.split('.').last)
+      url += ".gif"
+    end
+
+    log(:images, url)
+
+    [title, url]
+  end
+
+  Linkbot::Plugin.register('fwa', self, {
+    :message => {:regex => /!fwa/i, :handler => :on_message, :help => :help}
+  })
+end
