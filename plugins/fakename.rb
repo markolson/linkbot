24aeff647592ff5e577d46c23aa716de33e684bf
Bill Mill <bill.mill@gmail.com>
2013-07-24 13:58:45 -0400

    adding a fake name generator Wiener-style

---
+++ plugins/fakename.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+class Fakename < Linkbot::Plugin
+    def self.on_message(message, matches)
+      firstnames = ['Alberto','Alejandro','Alfonso','Andres','Aníbal','Antonio','Ariel','Armando','Benito','Bernardo','Carlos','Carmelo','Cesar','Dario','Diego','Eduardo','Efraín','Emilio','Enrique','Ernesto','Esteban','Fabricio','Federico','Feliciano','Felipe','Fernando','Gilberto','Guillermo','Gustavo','Ignacio','Inigo','Jaime','Javier','Jorge','José','José María','José Enrique','Juan','Julio','Julio Cesar','Leandro','Lorenzo','Luis','Manolo','Manuel','Marcelino','Marcelo','Marcos','Mariano','Mario','Mateo','Maximiliano','Miguel Ángel','Narciso','Omar','Osvaldo','Pablo','Paco','Pascual','Pepe','Raphael','Raul','Ricardo','Rolando','Salvador','Sandro','Santiago','Silvestre','Víctor','Victorino']
+      lastnames = ['Adventure','Badass','Calamity','Catastrophe','Clandestine','Covert','Death','Distress','Dynamite','Evil','Furtive','Gamble','Hazard','Jeopardy','Kill','Menace','Peril','Risk','Scourge','Sly','Smash','Stealth','Threat','Trouble','Verboten','Violence']
+
+      "#{firstnames.sample} #{lastnames.sample}"
+  end
+
+  def self.help
+    "!fakename - generate a fake name"
+  end
+
+  Linkbot::Plugin.register('fakename', self,
+    {
+      :message => {:regex => /!fakename/, :handler => :on_message, :help => :help},
+      :"direct-message" => {:regex => /!fakename/, :handler => :on_message, :help => :help}
+    }
+  )
+end
