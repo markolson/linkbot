d9b831f3e023fcc16cc8619ed22d09ab37c12baf
Bill Mill <bill.mill@gmail.com>
2013-11-26 11:45:35 -0500

    use giphy instead of google images for gif search

--- plugins/gif.rb
+++ plugins/gif.rb
@@ -13,10 +13,6 @@ class Gif < Linkbot::Plugin
     searchterm = matches[0]
     if searchterm.nil?
       searchterm = message_history(message)[0]['body']
-      if searchterm == "!image"
-        doc = Hpricot(open("http://www.randomword.net").read)
-        searchterm = doc.search("#word h2").text.strip
-      end
     end
 
     gifs = []
@@ -24,12 +20,14 @@ class Gif < Linkbot::Plugin
     begin
       # Give google 2 seconds to respond (and for us to parse it!)
       Timeout::timeout(2) do
-        searchurl = "https://www.google.com/search?hl=en&safe=on&biw=517&bih=1073&site=imghp&tbs=itp%3Aanimated&tbm=isch&sa=1&q=#{URI.encode(searchterm)}&oq=bananas&gs_l=img.3..0l10.1894734.1895319.0.1896030.7.6.0.1.1.1.109.435.5j1.6.0...0.0...1c.1.7.img.4-MDM6JAaIY"
+        # giphy uses dashes instead of %20 *shrug*
+        searchterm = searchterm.gsub(" ", "-")
+        searchurl = "http://giphy.com/search/#{URI.encode(searchterm)}"
 
-        gifs = open(searchurl).read.scan(/imgurl=(http:\/\/.*?)&/).flatten
+        gifs = open(searchurl).read.scan(/data-animated="(http:\/\/.*?)"/).flatten
       end
     rescue Timeout::Error
-      return "Google is slow! No gifs for you."
+      return "Giphy is slow! No gifs for you."
     end
 
     #funnyjunk sucks
