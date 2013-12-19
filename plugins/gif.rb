8f9744efb3b8c5a3f3f1e33db402f9ea58e4066c
Bill Mill <bill.mill@gmail.com>
2013-12-19 18:01:31 -0500

    use google for gifs again

--- plugins/gif.rb
+++ plugins/gif.rb
@@ -20,14 +20,14 @@ class Gif < Linkbot::Plugin
     begin
       # Give google 2 seconds to respond (and for us to parse it!)
       Timeout::timeout(2) do
-        # giphy uses dashes instead of %20 *shrug*
-        searchterm = searchterm.gsub(" ", "-")
-        searchurl = "http://giphy.com/search/#{URI.encode(searchterm)}"
+        searchurl = "https://www.google.com/search?site=imghp&tbs=itp:animated&tbm=isch&q=#{URI.encode(searchterm)}&oq=bananas&gs_l=img.3..0l10.112228.112858.0.113513.7.5.0.2.2.0.91.408.5.5.0....0...1c.1.32.img..0.7.416.dxM6ig7fTKY&bav=on.2,or.r_qf.&bvm=bv.58187178,d.eW0,pv.xjs.s.en_US.EeLgqkzqnSg.O&biw=1377&bih=1188&dpr=1&safe=active"
+        # this is an old iphone user agent. Seems to make google return good results.
+        useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7"
 
-        gifs = open(searchurl).read.scan(/data-animated="(http:\/\/.*?)"/).flatten
+        gifs = open(searchurl, "User-Agent" => useragent).read.scan(/http:\/\/[\/\.\w\-]*?\.gif/)
       end
     rescue Timeout::Error
-      return "Giphy is slow! No gifs for you."
+      return "Google is slow! No gifs for you."
     end
 
     #funnyjunk sucks
