8421a843689dc2740878e4d09de93b2b67b7c936
Robb Kidd <robb@thekidds.org>
2014-02-04 14:01:55 -0500

    Comply with Anglicized spelling standard.

--- plugins/bikeshed.rb
+++ plugins/bikeshed.rb
@@ -1,6 +1,6 @@
 # Inspired by bikeshed.io Bikeshedding-as-a-Service,
-# this plugin retrieves a color badge (color name, swatch and votes)
-# from colorlovers.com
+# this plugin retrieves a colour badge (colour name, swatch and votes)
+# from colourlovers.com
 
 class Bikeshed < Linkbot::Plugin
 
@@ -12,8 +12,8 @@ class Bikeshed < Linkbot::Plugin
   })
 
   def self.on_message(message, matches)
-    color = Nokogiri::XML(open('http://www.colourlovers.com/api/colors/random'))
-    color.xpath('//badgeUrl').children.first.content
+    colour = Nokogiri::XML(open('http://www.colourlovers.com/api/colors/random'))
+    colour.xpath('//badgeUrl').children.first.content
   end
 
 
