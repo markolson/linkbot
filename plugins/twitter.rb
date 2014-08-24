6269d50bf9f139d69649cba75c04d077a99d7a04
Bill Mill <bill.mill@gmail.com>
2014-08-24 16:37:55 -0400

    handle replies and images better

--- plugins/twitter.rb
+++ plugins/twitter.rb
@@ -7,9 +7,20 @@ class Twitter < Linkbot::Plugin
   })
 
   def self.on_message(message, matches)
-    puts "----------------------* herererererr *-------------------"
     url = matches[0]
     doc = Hpricot(open(url).read)
-    doc.at(".tweet-text").inner_text
+    msg = doc.at(".opened-tweet .tweet-text").inner_text
+
+    # images we want to display have a "src" and match pbs.twimg.com/media
+    images = doc.search("img").select {|x| x.attributes.to_hash.has_key?("src") && x["src"].match(/pbs.twimg.com\/media/) }
+    if images.length > 0
+      src = images.first.attributes["src"]
+      # strip trailing :large
+      src.gsub! /:large$/, ''
+
+      msg = "#{msg} #{src}"
+    end
+
+    msg
   end
 end
