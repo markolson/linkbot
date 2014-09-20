596aad3bad383cec871e4d98fdff5e6e81eb51fa
Bill Mill <bill.mill@gmail.com>
2014-09-20 18:22:40 -0400

    more plugin tests

--- spec/plugins_spec.rb
+++ spec/plugins_spec.rb
@@ -7,8 +7,9 @@ describe Linkbot::Plugin do
     Linkbot::Plugin.collect([PLUGIN_PATH])
   end
 
-  it "has one plugin" do
+  it "has the correct plugin" do
     expect(Linkbot::Plugin.plugins.keys.length).to eq 1
+    expect(Linkbot::Plugin.plugins['mock'][:ptr]).to eq MockPlugin
+    expect(Linkbot::Plugin.plugins['mock'][:handlers]).to eq({:message => {:regex => //, :handler => :on_message, :help => :help}})
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-require 'rspec'
+require 'byebug'
 require 'linkbot'
+require 'rspec'
 
 PLUGIN_PATH = File.expand_path(File.join(File.dirname(__FILE__), "plugins"))
