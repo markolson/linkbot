297a9cb5b8d34b2ae2d73a555ab2959a4845ca40
Robb Kidd <robb@thekidds.org>
2020-07-21 17:13:08 -0400

    declare results var in a higher scope
    ... so that the rest of the method can use it, eh?
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- plugins/image.rb
+++ plugins/image.rb
@@ -30,6 +30,8 @@ class Image < Linkbot::Plugin
     # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7"
 
+    results = ''
+
     begin
       Timeout::timeout(4) do
         results = HTTParty.get(searchurl, {
